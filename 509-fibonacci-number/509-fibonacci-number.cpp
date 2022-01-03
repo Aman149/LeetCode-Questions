790c356f2de61447bf60a0c4f7a8b88da5583cb8
Aman <aman001014@gmail.com>
2022-01-03 23:49:05 +0530

    Update 509-fibonacci-number.cpp

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,3 +1,4 @@
+//Recursive
 class Solution {
 public:
     int fib(int n) {
@@ -6,3 +7,23 @@ public:
         return (fib(n-1)+fib(n-2));
     }
 };
+
+-------------------OR---------------
+//Iterative
+class Solution {
+public:
+    int fib(int n) {
+        
+        if(n < 2) 
+            return n;
+        
+        int first = 0,  second = 1;
+        int ans = first+second;
+    
+        for(int i = 2; i < n; i++) {
+            first = second;
+            second = ans;
+            ans = first+second;
+        }
+        return ans;
+    }
