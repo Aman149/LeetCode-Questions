b8f4dc4a368535420c9857699b0b563c20b4befb
Aman <63321970+Aman149@users.noreply.github.com>
2022-07-07 21:41:38 +0530

    Time: 20 ms (97.99%), Space: 23.8 MB (93.28%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        //O(1) Space
-        //O(n) Time
+        //O(1) Space, O(n) Time Complexity
         
+        //ans variable to store the product of array. 
         int ans = 1;
+        //zeroPos to store position of zero, CountZeros to count if there are more than one zero or not
         int zeroPos = -1, countZeros = 0;
         
+        //Iterate and calc. prod. countzero and pos of zero
         for(int i=0; i<nums.size(); i++) {
             
             if(nums[i] != 0)
@@ -17,9 +19,11 @@ public:
             }       
         }
         
+        //if zero exists
         if(zeroPos != -1) {
             fill(nums.begin(), nums.end(), 0);
             
+            //if there are more than one zero i.e the ans array will only contain zeros
             if(countZeros > 1)
                 return nums;
             else
@@ -27,6 +31,7 @@ public:
                 return nums;
         }
             
+        //If no zeros exists then then calculate. 
         for(int i=0; i<nums.size(); i++)
             nums[i] = ans/nums[i];        
         
