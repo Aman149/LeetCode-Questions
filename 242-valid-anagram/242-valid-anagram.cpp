9e4a21874239027fd9c060cea31d705601856697
Aman <63321970+Aman149@users.noreply.github.com>
2022-08-12 09:00:08 +0530

    Time: 7 ms (87.59%), Space: 7.2 MB (94.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n = s.size(), m = t.size() ;
-        
-        //base case if sizes of both strings are not same
-        if(n != m)
+        //base cases
+        if(s == t)
+            return true;
+        if(s.size() != t.size())
             return false;
+        //Create an array of size 26 (English Alphabets)
+        //Add from s and subtract from t
+        int arr[26] = {};
+        for(int i=0; i<s.size(); i++) {
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
         
-        //add them to unordered map
-        unordered_map<char,int> t1, t2;
-        for(char it: s)
-            t1[it]++;
-        for(char it: t)
-            t2[it]++;
-        
-        //check if the occurances are same or not
-        for(int i=0; i<n; i++) {
-            if(t1[s[i]] != t2[s[i]])
+        for(int i=0; i<26; i++) {
+            if(arr[i])
                 return false;
         }
         return true;
     }
+    
 };
