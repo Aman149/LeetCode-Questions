b0ee74d3954c456270bbdb84ea6991b15db5268d
Aman <63321970+Aman149@users.noreply.github.com>
2022-09-28 22:20:23 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (75.00%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,38 +11,44 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
         ListNode* curr = head, *prev = head;
         int size = 0;
         
+        //TO find the size of the LL
         while(curr != NULL) {
             curr = curr->next;
             size++;
         }
         
-        if(size == 1) {
+        //Case 1 when only one element is present in LL
+        if(size == 1)
             return NULL;
-        }
         
+        //Case 2, If the element we want to remove is the
+        //first element of the LL
         if(size == n) {
-            ListNode * temp = head;
+            ListNode *temp = head;
             head = head->next;
             temp->next = NULL;
             return head;
         }
         
+        //Case 3, If the element is present in the LL
+        //excluding the 1st position i.e. n != size of LL
+        //That means we have to iterate and find the element that
+        //wa have to remove, for this we can use tho 2 pointer approach
         curr = head;
         curr = curr->next;
-        int i = size - n;
-        while(curr != NULL && i>1) {
-            curr= curr->next;
+        int i = size-n;
+        while(curr != NULL && i > 1) {
+            curr = curr->next;
             prev = prev->next;
             i--;
         }
-        
+        //Now curr stores the element that we want to remove
+        //And prev stores the previous element
         prev->next = curr->next;
         
         return head;
-        
     }
 };
