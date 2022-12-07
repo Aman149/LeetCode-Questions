a50d1e51b73db5591a9eacf7f1c7256a5f0dff67
Aman <63321970+Aman149@users.noreply.github.com>
2022-12-07 11:59:11 +0530

    Time: 157 ms (79.75%), Space: 64.7 MB (18.63%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -9,29 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution 
+{
 public:
-    int rangeSumBST(TreeNode* root, int low, int high) {
+    void inorder(TreeNode* node, int &low, int &high, int &sum)
+    {
+        if(!node) return;
+        inorder(node->left, low, high, sum);
+        if(node->val >= low && node->val <= high) sum += node->val;
+        inorder(node->right, low, high, sum);
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) 
+    {
         int sum=0;
-        if(root == NULL)
-            return sum;
-        
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        while(q.empty() == false) {
-            TreeNode* curr = q.front();
-            q.pop();
-            
-            if(curr->val >= low && curr->val <= high)
-                sum += curr->val;
-            
-            if(curr->left)
-                q.push(curr->left);
-            if(curr->right)
-                q.push(curr->right);
-        }
-        
-        return sum;
+        inorder(root, low, high, sum);
+        return sum;  
     }
 };
