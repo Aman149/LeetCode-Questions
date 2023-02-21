be0f23aa49826cfdd883cc641a1fb300aee3c330
Aman <63321970+Aman149@users.noreply.github.com>
2023-02-22 00:02:07 +0530

    Time: 33 ms (37.69%), Space: 22.4 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int left = 0, right = nums.size() - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (mid % 2 == 1) {
+    int singleNonDuplicate(vector<int>& arr) {
+        int low = 0, high = arr.size()-1, mid;
+        
+        while(low<high) {
+            mid = (low+high)/2;
+            
+            //If mid is odd make it even
+            //as the element at even position and the element at position+1 
+            //must be same if the ans is present in right half of the array
+            if(mid%2 == 1)
                 mid--;
-            }
-            if (nums[mid] != nums[mid + 1]) {
-                right = mid;
-            } else {
-                left = mid + 2;
-            }
+            
+            //The element is present in left half
+            if(arr[mid] != arr[mid+1])
+                high = mid;
+            
+            else
+                low = mid + 2;
+                
         }
-        return nums[left];
+        return arr[low];
     }
 };
