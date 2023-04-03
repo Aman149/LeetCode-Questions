56e2d135a59f41ea990f6c4dfd74a2e171b5b816
Aman <63321970+Aman149@users.noreply.github.com>
2023-04-04 01:04:53 +0530

    Time: 80 ms (88.31%), Space: 44.7 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(), people.end());
+        vector<int> count(limit+1, 0);
+        for(auto p : people) count[p]++;
+        
+        int idx = 0;
+        for(auto val = 1; val<= limit; val++){
+            while(count[val]-- >0)
+                people[idx++] = val;
+        }
+        
         int low = 0, high = people.size() - 1;
-        int count = 0;
+        int boatCount = 0;
         while(low <= high) {
             if(people[low]+people[high] <= limit) {
                 low++;
@@ -12,8 +20,8 @@ public:
             else {
                 high--;
             }
-            count++;
+            boatCount++;
         }
-        return count;
+        return boatCount;
     }
 };
