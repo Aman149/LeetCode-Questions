29059b68890a6db0cf81902db8162c775e0358d6
Aman <63321970+Aman149@users.noreply.github.com>
2023-04-04 00:58:09 +0530

    Time: 92 ms (43.45%), Space: 42 MB (11.29%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int low = 0, high = people.size() - 1;
+        int count = 0;
+        while(low <= high) {
+            if(people[low]+people[high] <= limit) {
+                low++;
+                high--;
+            }
+            else {
+                high--;
+            }
+            count++;
+        }
+        return count;
+    }
+};
