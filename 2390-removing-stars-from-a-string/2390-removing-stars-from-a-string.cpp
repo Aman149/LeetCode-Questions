5d347ae4493a6dc749c43384761c799ffd12b96c
Aman <63321970+Aman149@users.noreply.github.com>
2023-04-11 22:59:41 +0530

    Time: 90 ms (54.73%), Space: 25.9 MB (72.25%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans = "";
+        
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] != '*') 
+                ans += s[i];
+            else {
+                if(ans.size() == 0)
+                    return ans;
+                else
+                    ans.pop_back();
+            }
+        }
+        return ans;
+    }
+};
