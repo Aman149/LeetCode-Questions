240232fcdc7d0b9eb8a12138fa4a6cc371b4893e
Aman <63321970+Aman149@users.noreply.github.com>
2023-04-17 12:35:18 +0530

    Time: 4 ms (61.53%), Space: 8.9 MB (86.24%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -4,9 +4,7 @@ public:
         int maxCandie = *max_element(candies.begin(), candies.end());
         vector<bool> ans;
         for(auto candie : candies) {
-            if(maxCandie != candie && candie+extraCandies >= maxCandie)
-                ans.push_back(true);
-            else if(maxCandie == candie)
+            if(candie+extraCandies >= maxCandie)
                 ans.push_back(true);
             else
                 ans.push_back(false);
