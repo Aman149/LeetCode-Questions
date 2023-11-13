1fcd0652a488cd26f3322ad61ffb815c6c7cebae
Aman <63321970+Aman149@users.noreply.github.com>
2023-11-13 09:51:28 +0530

    Time: 42 ms (52.39%), Space: 12.7 MB (60.63%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<int> positions ;
-        vector<char> arrayVowles;
-        unordered_set<char> vowles;
-        
-        for (char c : {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'}) {
-        vowles.insert(c);
-    }
-        
-        for(int i=0; i<s.size(); i++) {
-            if(vowles.find(s[i]) != vowles.end()) {
-                positions.push_back(i);
-                arrayVowles.push_back(s[i]);
+        // use a hash table to check whether a character is a vowel or not
+        const unordered_set<char> vowels = {
+            'a', 'e', 'i', 'o', 'u',
+            'A', 'E', 'I', 'O', 'U',
+        };
+
+        // count the number of each vowel
+        map<char, int> vowel_count;
+        for (auto& c : s) {
+            if (vowels.count(c) != 0) vowel_count[c]++;
+        }
+
+        // replace the vowels in the nondecreasing order
+        for (char& c : s) {
+            if (vowels.count(c) != 0) {
+                auto it = vowel_count.begin();
+                c = it->first;
+                if (--(it->second) == 0) vowel_count.erase(it);
             }
         }
-        
-        sort(arrayVowles.begin(), arrayVowles.end());
-        
-        for(int i=0; i<positions.size(); i++) {
-            s[positions[i]] = arrayVowles[i];
-        }
-        
         return s;
     }
 };
