d16990b02def335c3795ea5259edfecb59c974bc
Aman <63321970+Aman149@users.noreply.github.com>
2024-01-02 08:39:48 +0530

    Time: 37 ms (5.35%), Space: 29.8 MB (92.71%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        int distinct[201] = {0};
-        int size = nums.size();
+        vector<int> freq(nums.size() +1);
         
-        for(auto num : nums) {
-            distinct[num]++;
+        vector<vector<int>> ans;
+        for(int c : nums) {
+            if(freq[c] >= ans.size())
+                ans.push_back({});
+            
+            ans[freq[c]].push_back(c);
+            freq[c]++;
         }
         
-        vector<vector<int>> result;
-
-        while(size > 0) {
-            
-            vector<int> resRow;
-            for(int i=0; i<201; i++) {
-                if(distinct[i] != 0) {
-                    resRow.push_back(i);
-                    distinct[i]--;
-                    size--;
-                }
-            }
-            
-            result.push_back(resRow);
-        }
-        
-        return result;
+        return ans;
     }
 };
